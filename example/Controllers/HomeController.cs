b4e29a298186b649cad5fa3d0422325ef624d993
Jonathan Olesker <jonathan.olesker@audacia.co.uk>
2026-05-06 11:38:51 +0100

    feat: perform dotnet 10 upgrade and convert to file-scoped namespaces #204012

--- example/Controllers/HomeController.cs
+++ example/Controllers/HomeController.cs
@@ -1,25 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using example.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using Microsoft.AspNetCore.Mvc;
-using example.Models;
 
-namespace example.Controllers
+namespace example.Controllers;
+
+[Route("/")]
+public class HomeController : Controller
 {
-    [Route("/")]
-    public class HomeController : Controller
+    // GET
+    [HttpGet]
+    public IEnumerable<string> Get()
     {
-        // GET
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return HttpContext.Response.Headers.Select(h => h.ToString()).ToArray();
-        }
+        return HttpContext.Response.Headers.Select(h => h.ToString()).ToArray();
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
